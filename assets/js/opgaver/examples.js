b2a54782ab8488cf503d2b7ff129b6fa87023cba
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-08-25 13:33:43 +0200

    update function updateExtras needs to be done

--- assets/js/opgaver/examples.js
+++ assets/js/opgaver/examples.js
@@ -9,16 +9,16 @@ myExtrasInput.addEventListener("change", addExtra);
 
 function addExtra() {
     /* din map kode her... */
+    myExtras = myExtrasInput.value.split(',').map(item => item.trim());
+    myExtrasListElement.innerHTML = myExtras.map(item => `<li>${item}</li>`).join('');
 }
 
 
 
 function updateExtras() {
 
-
-
-
     /* din map kode her... */
+
 
 
 }
